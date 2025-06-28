3bd6f656b101ac2565fcc105ad6bd7577ca6e669
s5935512064 <32189965+s5935512064@users.noreply.github.com>
2025-06-29 02:12:40 +0700

    add sitemap

---
+++ src/pages/sitemap.xml.ts
@@ -0,0 +1,18 @@
+export const prerender = true;
+
+export async function GET() {
+  const xml = `<?xml version="1.0" encoding="UTF-8" ?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>https://theoldsiam.co.th/event/2025/pride-month-vote</loc>
+      <changefreq>monthly</changefreq>
+      <priority>0.8</priority>
+    </url>
+  </urlset>`;
+
+  return new Response(xml, {
+    headers: {
+      "Content-Type": "application/xml",
+    },
+  });
+}
