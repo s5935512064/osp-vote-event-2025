f584e0b0861b578dd6a1f01174a554bad586b2b8
s5935512064 <32189965+s5935512064@users.noreply.github.com>
2025-08-05 21:13:58 +0700

    add slug /event

--- src/lib/eventsConfig.ts
+++ src/lib/eventsConfig.ts
@@ -8,7 +8,7 @@ export const events: Event[] = [
     subtitle: "ร่วมแสดงความรักและความกตัญญูต่อคุณแม่",
     description:
       "12 สิงหาคมปีนี้ ให้ ดิโอลด์ สยาม พลาซ่า เป็นสื่อกลางแทนความรักและความรู้สึกอันแสนอบอุ่นที่คุณมีต่อคุณแม่ ร่วมกิจกรรมพิเศษและชมคอลเลกชันผ้าไหมไทยสุดพิเศษ พร้อมรับของที่ระลึกสำหรับคุณแม่ที่รัก",
-    href: "/2025/mothers-day-activity",
+    href: "/event/2025/mothers-day-activity",
     image: MothersDayImage,
     status: "active",
     startDate: "2025-08-08",
@@ -29,10 +29,10 @@ export const events: Event[] = [
     title: "The Old Siam: We're Proud of You",
     subtitle: "ผ้าไหมไทยผ่านความภาคภูมิใจของ ดิ โอลด์ สยาม",
     image:
-      "https://res.cloudinary.com/dndcgytjh/image/upload/v1752777677/ejowcreczyfw5ui42ztj.webp",
+      "https://assets-manager.ssdapp.net/public/file/ppmfyud6wyaj8fjuvz4koui7",
     description:
       "ร่วมโหวตความงามของชุดผ้าไหมไทยภายใน ดิ โอลด์ สยาม พลาซ่า เพื่อสนับสนุนเอกลักษณ์ และอนุรักษ์สมบัติทางวัฒนธรรมไทย",
-    href: "/2025/pride-month-vote",
+    href: "/event/2025/pride-month-vote",
     status: "active",
     startDate: "2025-07-01",
     endDate: "2025-08-17",
